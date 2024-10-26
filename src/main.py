b7ab2664e949cedffa4af875a2bb5b21d128181f
maj0r <sebastian.lesniewski.edu@gmail.com>
2024-10-26 19:19:12 +0200

    Adjust template

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from .routers import example
 
 app = FastAPI(
-    title='renameme',
+    title='knowledge-graph',
     description='Fill the description',
     version='0.1',
 )
